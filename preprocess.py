96fc1bd5b1db51cd6c1f4c629ed58b4acecfd720
manjubura <100593885+manjubura@users.noreply.github.com>
2024-10-26 15:24:32 +0100

    preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,1 +1,28 @@
+import pandas as pd
+import ast
 
+def load_data(country):
+    """
+    Load and preprocess data for a given country to extract specific columns.
+    
+    Parameters:
+        country (str): The country name to load data for, as specified in the file name.
+        
+    Returns:
+        dict: Dictionary containing extracted 'body', 'title', 'source', 'date' as lists.
+    """
+    
+  # Define the file path based on the country
+    file_path = f"data/all_{country.lower()}.xlsx"
+    
+    # Load the Excel file
+    df = pd.read_excel(file_path)
+    
+    data = {
+        f"{country.lower()}body": df['body'],
+        f"{country.lower()}title": df['title'],
+        f"{country.lower()}date": df['date'],  
+        f"{country.lower()}source": df['source'].apply(lambda x: ast.literal_eval(x)['title'])
+    }
+    
+    return data
